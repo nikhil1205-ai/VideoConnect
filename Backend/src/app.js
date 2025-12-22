38c3840fa66088a0cfcc52131f775639764fc089
Nikhil dangi <nikhildangi1205@gmail.com>
2025-12-22 19:18:59 +0530

    resolve error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,10 @@ app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true}));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://videoconnect-qgb9.onrender.com"
+    ],
     credentials: true,
   })
 );
