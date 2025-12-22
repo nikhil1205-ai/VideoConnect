b03d209355ab272dcc0375468ec1a9cfd8513502
Nikhil dangi <nikhildangi1205@gmail.com>
2025-12-22 16:34:35 +0530

    Deployment

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? "https://videoconnect-qgb9.onrender.com"
+    : "http://localhost:8080";
+
 export default axios.create({
-   baseURL:"http://localhost:8080",
-   withCredentials:true
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
